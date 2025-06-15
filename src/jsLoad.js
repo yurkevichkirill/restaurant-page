539b9a9d4513c3cbd4090c4e92fa07044cf49799
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-06-15 20:48:07 +0300

    Create all info only with js

--- src/jsLoad.js
+++ src/jsLoad.js
@@ -7,6 +7,7 @@ function createJS(){
 
     const header = document.createElement("div");
     header.className = "header";
+    header.textContent = "Welcome to our Bear Restaurant";
     content.appendChild(header);
 
     const left_sidebar = document.createElement("div");
@@ -61,61 +62,81 @@ function createJS(){
     fries.appendChild(friesImg);
 
     const friesText = document.createElement("div");
-    friesText.textContent = "Juicy Nuggggets";
+    friesText.textContent = "Tasteful Fries";
     fries.appendChild(friesText);
+
+    const about = document.createElement("div");
+    about.className = "about";
+    article.appendChild(about);
+
+    const about_head = document.createElement("div");
+    about_head.className = "about-head";
+    about_head.textContent = "Some facts about";
+    about.appendChild(about_head);
+
+    const text_about = document.createElement("div");
+    text_about.className = "text-about";
+    text_about.textContent = "Nestled in the heart of Grove Street 69, our restaurant is a cozy den for food lovers with a taste for adventure. Inspired by the mighty bear—a symbol of strength, warmth, and hearty appetites—we serve up delicious, satisfying meals that’ll make you growl for more!";
+    about.appendChild(text_about);
+
+    const rigth_sidebar = document.createElement("div");
+    rigth_sidebar.className = "right-sidebar";
+    content.appendChild(rigth_sidebar);
+
+    const footer = document.createElement("div");
+    footer.className = "footer";
+    content.appendChild(footer);
+
+    const contact = document.createElement("div");
+    contact.className = "contact";
+    footer.appendChild(contact);
+    
+    const contact_head = document.createElement("div");
+    contact_head.className = "contact-head";
+    contact_head.textContent = "CONTACT";
+    contact.appendChild(contact_head);
+
+    const address = document.createElement("div");
+    address.className = "address";
+    address.innerHTML = "69-Grove Street<br>Madrid, Spain";
+    contact.appendChild(address);
+    
+    const mail = document.createElement("div");
+    mail.className = "mail";
+    contact.appendChild(mail);
+
+    const mailLink = document.createElement("a");
+    mailLink.textContent = "kiril.iurkievich@mail.ru";
+    mailLink.href = "#";
+    mail.appendChild(mailLink);
+
+    const phone = document.createElement("div");
+    phone.classList = "phone";
+    phone.textContent = "(8029)573-39-77";
+    contact.appendChild(phone);
+
+    const foot_head = document.createElement("div");
+    foot_head.className = "foot-head";
+    footer.appendChild(foot_head);
+
+    const name = document.createElement("div");
+    name.className = "name";
+    name.textContent = "Bear Restaurant";
+    foot_head.appendChild(name);
+
+    const hours = document.createElement("div");
+    hours.className = "hours";
+    footer.appendChild(hours);
+
+    const hours_head = document.createElement("div");
+    hours_head.className = "hours-head";
+    hours_head.textContent = "HOURS";
+    hours.appendChild(hours_head);
+
+    const schedule = document.createElement("div");
+    schedule.className = "schedule";
+    schedule.innerHTML = "Mon-Fri 16-02<br>Sut-Sun 14-04";
+    hours.appendChild(schedule);    
 }
 
 export {createJS};
-
-{/* <div id="content">
-        <div class="header">Welcome to our Bear Restaurant</div>
-        <div class="left-sidebar"></div>
-        <div class="article">
-            <div class="meals">
-                <div class="meal-head"> Our popular meals</div>
-                <div class="burger">
-                    <img src="imgs/burger.png" alt="Burger">
-                    <div>Tastyyy ChickenBurger</div>
-                </div>
-                <div class="nuggets">
-                    <img src="imgs/nuggets.png" alt="Nuggets">
-                    <div>Juicy Nuggggets</div>
-                </div>
-                <div class="fries">
-                    <img src="imgs/fries.png" alt="Fries">
-                    <div>Tasteful Fries</div>
-                </div>
-            </div>
-            <div class="about">
-                <div class="about-head">Some facts about</div>
-                <div class="text-about">Nestled in the heart of Grove Street 69, our restaurant is a cozy den for food lovers with a taste for adventure. Inspired by the mighty bear—a symbol of strength, warmth, and hearty appetites—we serve up delicious, satisfying meals that’ll make you growl for more! </div>
-            </div>
-            <div class="work-time"></div>
-            <div class="location"></div>
-        </div>
-        <div class="right-sidebar"></div>
-        <div class="footer">
-            <div class="contact">
-                <div class="contact-head">CONTACT</div>
-                <div class="address">
-                    69-Grove Street<br>
-                    Madrid, Spain
-                </div>
-                <div class="mail">
-                    <a href="#">kiril.iurkievich@mail.ru</a>
-                </div>
-                <div class="phone"> (8029)573-39-77</div>
-            </div>
-            <div class="foot-head">
-                <div class="name">Bear Restaurant</div>
-                <div class="social"></div>
-            </div>
-            <div class="hours">
-                <div class="hours-head">HOURS</div>
-                <div class="schedule">
-                    Mon-Fri 16-02<br>
-                    Sut-Sun 14-04
-                </div>
-            </div>
-        </div>
-    </div> */}
